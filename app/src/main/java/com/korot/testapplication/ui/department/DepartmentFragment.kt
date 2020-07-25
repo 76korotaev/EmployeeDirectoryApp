78a79e09d426b068fc658f024d4b7bd1e80f1bb9
Andrey Korotaev <akorotaev@sdvor.com>
2020-07-25 19:02:23 +0500

    ADD department adapter

--- app/src/main/java/com/korot/testapplication/ui/department/DepartmentFragment.kt
+++ app/src/main/java/com/korot/testapplication/ui/department/DepartmentFragment.kt
@@ -6,22 +6,28 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.korot.testapplication.R
 import com.korot.testapplication.ui.MainActivity
 import com.korot.testapplication.ui.base.BaseFragment
 import com.korot.testapplication.ui.base.ConsumerFragmentProvider
 import com.korot.testapplication.ui.base.LoaderInterface
+import com.korot.testapplication.ui.department.model.DepartmentItem
 
 class DepartmentFragment(fragmentProvider: ConsumerFragmentProvider): BaseFragment(fragmentProvider) {
 
     lateinit var viewList: RecyclerView
     private val model = DepartmentViewModel(loader)
+    private var adapter: DepatrmentAdapter = DepatrmentAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         model.observer.observe(this, Observer {
-            //todo тут в адаптер будем записывать
+            viewList.layoutManager =
+                LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
+            viewList.adapter = adapter
+            adapter.setItems(listOf(DepartmentItem(department = it)))
         })
         model.loadEmployee()
     }
--- app/src/main/java/com/korot/testapplication/ui/department/DepatrmentAdapter.kt
+++ app/src/main/java/com/korot/testapplication/ui/department/DepatrmentAdapter.kt
@@ -1,15 +1,17 @@
 package com.korot.testapplication.ui.department
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
 import com.korot.testapplication.R
 import com.korot.testapplication.domain.model.Department
+import com.korot.testapplication.domain.model.Employee
 import com.korot.testapplication.ui.department.model.DepartmentItem
 import java.lang.Exception
-import kotlin.math.E
 
 class DepatrmentAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
@@ -26,10 +28,26 @@ class DepatrmentAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
                 }
             }
         }
-
     }
 
-    private val items : MutableList<DepartmentItem> = ArrayList<DepartmentItem>()
+    private val items : MutableList<DepartmentItem> = ArrayList()
+
+    fun addItemsInParent(childs: List<DepartmentItem>, idParent: Int){
+        val parentDep = this.items.firstOrNull { it.department?.id == idParent }
+        if (parentDep!= null){
+            val index = items.indexOf(parentDep) + 1
+            items.addAll(index, childs)
+            notifyItemRangeInserted(index, childs.size)
+        }
+    }
+
+    fun openDepartment(depatrment: Department){
+        if (!depatrment.department.isNullOrEmpty()){
+            addItemsInParent(depatrment.department.map { DepartmentItem(department = it) }, depatrment.id)
+        } else {
+            addItemsInParent(depatrment.employees.map { DepartmentItem(employee = it) }, depatrment.id)
+        }
+    }
 
     fun setItems(items: List<DepartmentItem>){
         this.items.clear()
@@ -44,29 +62,60 @@ class DepatrmentAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
             item.employee != null -> DepartmentItemType.EMPLOYEE.id
             else -> -1
         }
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        val type = DepartmentItemType.valuewOf(viewType)
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(type.layoutId, parent, false)
+        return when(type){
+            DepartmentItemType.DEPARTMENT -> DepatrmentViewHolder(view)
+            DepartmentItemType.EMPLOYEE -> EmployeeViewHolder(view)
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return items.size
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = items[position]
+        when(holder){
+            is DepatrmentViewHolder -> holder.bind(item.department)
+            is EmployeeViewHolder -> holder.bind(item.employee)
+        }
     }
 
-    inner class DepatrmentViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    inner class DepatrmentViewHolder(view: View): RecyclerView.ViewHolder(view){
 
         private val viewName : AppCompatTextView = view.findViewById(R.id.view_list_department_name)
         private val viewId : AppCompatTextView = view.findViewById(R.id.view_list_department_id)
 
-        fun bind(depatrment: Department){
-            viewName.setText(depatrment.name)
-            viewId.setText(depatrment.id.toString())
+        fun bind(depatrment: Department?){
+            viewName.setText(depatrment?.name)
+            viewId.setText(depatrment?.id.toString())
+            itemView.setOnClickListener {
+                depatrment?.let { notNullDepartment ->
+                    openDepartment(notNullDepartment)
+                }
+            }
+        }
+    }
+
+    inner class EmployeeViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+        private val viewName : AppCompatTextView = view.findViewById(R.id.view_list_employee_name)
+        private val viewId : AppCompatTextView = view.findViewById(R.id.view_list_employee_id)
+        private val viewIcon : AppCompatImageView = view.findViewById(R.id.view_list_employee_icon)
+
+        fun bind(employee: Employee?){
+            viewName.setText(employee?.name)
+            viewId.setText(employee?.id.toString())
+
+        }
+
+        fun loadImage(url: String){
+
         }
     }
 
